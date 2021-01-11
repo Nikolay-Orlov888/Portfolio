28d1df475fe4f3a80ebe817f53fb4e9ecc25f0b0
Nikolay <nvorlov99@gmail.com>
2021-01-11 18:02:46 +0300

    Header mobile ready

--- docs/js/main.js
+++ docs/js/main.js
@@ -86,9 +86,31 @@ $(document).ready(function(){
 })
 
 const toggleMenu = document.querySelector('.toggle-menu'); //иконка гамбургер
+const mobMenu = document.querySelector('.mobile-menu'); //mob menu
+const overlay = document.querySelector('#overlay'); //overlay
+const bodyEl = document.body; //
+
 
 //прослушиваем событие клик по гамбургеру
 toggleMenu.addEventListener('click', function(){
-	
 	this.classList.toggle('active'); //  переключаем класс active  у гамбургера
+	mobMenu.classList.toggle('active'); //  переключаем класс active  у mobMenu
+	overlay.classList.toggle('active'); //  переключаем класс active  у overlay
+	bodyEl.classList.toggle('noscroll'); //  переключаем класс noscroll  у body
 });
+
+//прослушиваем событие клик по mob menu
+mobMenu.addEventListener('click', function(){
+	this.classList.remove('active');
+	toggleMenu.classList.remove('active');
+	overlay.classList.remove('active');
+	bodyEl.classList.remove('noscroll');
+})
+
+//прослушиваем событие клик по overlay
+overlay.addEventListener('click', function(){
+	this.classList.remove('active');
+	toggleMenu.classList.remove('active');
+	mobMenu.classList.remove('active');
+	bodyEl.classList.remove('noscroll');
+})
